665e2b9c00a43bcde9102e0c811138351e70f3ba
Bryn Thomas <bryn500@outlook.com>
2016-07-15 15:37:29 +0100

    Add files via upload

---
+++ jquery.characterCount.js
@@ -0,0 +1,45 @@
+﻿/* globals jQuery */
+(function ($) {
+    'use strict';
+
+    $.fn.characterCount = function (options) {
+        var self = this,
+            settings = $.extend({
+                // defaults
+                className: 'charRemaining'
+            }, options);
+
+        function charactersRemaining(textarea) {
+            var textContent = textarea.val(),
+                maxCharacters = parseInt(textarea.attr('maxlength'), 10),
+                characters = textContent.length,
+                charactersRemaining = maxCharacters - characters,
+                charMessage = textarea.next('.' + settings.className);
+
+            if (charactersRemaining === 1) {
+                charMessage.text(charactersRemaining + ' character remaining');
+            } else {
+                charMessage.text(charactersRemaining + ' characters remaining');
+            }
+        }                
+
+        (function init() {
+            self.bind('input selectionchange propertychange', function (event) {
+                var that = $(event.currentTarget);
+                charactersRemaining(that);
+            });
+
+            self.each(function (index, value) {
+                var element = $("<span></span>").addClass(settings.className);
+
+                if (!self.next('.' + settings.className).length) {
+                    self.after(element);
+                }
+
+                charactersRemaining($(value));
+            })
+        }());
+
+        return self;
+    };
+}(jQuery));
